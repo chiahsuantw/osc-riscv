d5ea616b534d80730ef7d1da8ef23a20a0f62a2c
Chia-Hsuan Lin <chlin.cs12@nycu.edu.tw>
2024-09-10 15:23:04 +0800

    Trap handling

--- src/main.c
+++ src/main.c
@@ -2,10 +2,24 @@
 #include "printk.h"
 #include "shell.h"
 
+void switch_to_user_mode()
+{
+    asm("csrw sepc, ra;");
+    asm("sret");
+}
+
+void trap_handler()
+{
+    printk("Kernel Trap!\n");
+    while (1)
+        ;
+}
+
 int start_kernel()
 {
     printk("\nNYCU OSC RISC-V KERNEL\n");
-    fdt_traverse("linux,initrd-start", 0);
+    switch_to_user_mode();
+    asm("ecall");
     run_shell();
     return 0;
 }
